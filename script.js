f0635d467502dd1fa702dd2824a3a9b36855f0ee
Uyearit <uyearit23@gmail.com>
2025-08-27 22:00:19 +0100

    feat: Add Event Listeners to navigate to watch pages.

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-const navButton = document.querySelector('.nav-button');
+const navButton = document.querySelectorAll('.nav-button');
+const screenOverlay = document.querySelector('.screen-overlay');
+const themeButton = document.querySelector('.theme-button i');
+
+if (localStorage.getItem('darkMode') === 'enabled') {
+  document.body.classList.add('dark-mode');
+  themeButton.classList.replace('fa-moon', 'fa-sun');
+} else {
+  themeButton.classList.replace('fa-sun', 'fa-moon');
+}
+//Toggle dark monde when theme buttons is clicked
+themeButton.addEventListener('click', () => {
+  const isDarkMode = document.body.classList.toggle('dark-mode');
+  localStorage.setItem('darkMode', isDarkMode ? 'enabled' : 'disabled');
+  themeButton.classList.toggle('fa-sun', isDarkMode);
+  themeButton.classList.toggle('fa-moon', isDarkMode);
+});
 
 //Toggle sidebar visibility when nav buttons are clicked
-navButton.addEventListener('click', () => {
+navButton.forEach(button => {
+  button.addEventListener('click', () => {
+    document.body.classList.toggle('sidebar-hidden');
+  });
+});
+
+//Toggle sidebar visibility when screen overlay is clicked
+screenOverlay.addEventListener('click', () => {
   document.body.classList.toggle('sidebar-hidden');
 });
+
+if (window.innerWidth >= 768) {
+  document.body.classList.remove('sidebar-hidden');
+}
